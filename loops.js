88ce97b906358162cd2259940f5680b2a982812e
Isabella de Medici <bellamedici242@gmail.com>
2018-12-10 13:22:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,30 +1,11 @@
 function forLoop(array) {
-  for (var counter = 0; counter < 25; counter = counter + 1) {
-    if (counter==1) {
-      array.push("I am 1 strange loop.");  
+  for (i = 0; i < 25; i ++) {
+    if(i == 1) {
+      array.push("I am 1 strange loop.");
     }
     else {
-      array.push(`I am ${counter} strange loops.`);
+      array.push(`I am ${i} strange loops.`);
     }
   }
-  return array;
-}
-
-function whileLoop(number) {
-  while (number > 0) {
-    console.log(--number);
-  }
-   return "done"
-}
-
- var i = 0;
-function incrementVariable() {
-  i = i + 1;
-}
-
-function doWhileLoop(array) {
-  do {
-    array.pop();
-  } while (array.length > 0 && incrementVariable());
-  return array;
+    return array;
 }
